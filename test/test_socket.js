d6f30d7c216af62c55af8e0d85345836272a2f12
Bereket Abraham <babraham42@gmail.com>
2015-03-30 04:24:41 +0000

    serialport update

---
+++ test/test_socket.js
@@ -0,0 +1,36 @@
+// Socket.io client to collect and send light commands to the light table
+// Sits on the raspberry pi between the server and arduino microcontroller
+//
+// Bereket Abraham
+
+var io = require('socket.io-client');
+
+// Use with Express 3/4 or standalone server. use io() for http server
+var socket = io.connect('http://bereketabraham.com:8080');
+
+// comm API
+// 'initial_state', 'local_update' => 'remote_updates', 'remote_update'
+socket.on('connect', function(msg) {
+    console.log('connected: ' + JSON.stringify(msg));
+});
+
+socket.on('remote_update', function(msg) {
+    console.log('remote_update: ' + msg);
+});
+
+socket.on('remote_updates', function(msg) {
+    console.log('remote_updates: ' + msg);
+});
+
+socket.on('disconnect', function(msg) {
+    console.log('disconnected: ' + JSON.stringify(msg));
+});
+
+socket.on('error', function(msg) {
+    console.log('errored: ' + JSON.stringify(msg));
+});
+
+// get initial state of table
+socket.emit('initial_state', 'qq');
+
+
