cdcb3c280139c40b6c08c624ae58d6d015bd488a
Bereket Abraham <babraham@appnexus.com>
2015-10-17 05:28:16 -0400

    fixed server, client js files

--- socketio_server.js
+++ socketio_server.js
@@ -3,45 +3,50 @@
 // 
 // Bereket Abraham
 
-var port = 8002;
-var numLed = -1;
-var debug = false;
-
-var idx = process.argv.indexOf("--port");
-if (idx >= 0 && idx < process.argv.length) {
-    port = parseInt( process.argv[idx + 1] );
-    console.log("Starting on port " + port.toString());
-}
-//console.log(JSON.stringify(process.argv));
-
 var io         = require('socket.io').listen(port),
-    serialPort = require('serialport');
+    serialPort = require('serialport'),
     sleep      = require('sleep');
 
-////////////////// Data structure to hold light values
+var port       = 8002,
+    debug      = false,
+    lenr       = 8,
+    lenc       = 13,
+    numLed     = lenr * lenc,
+    background = '#22CCCC',
+    colorArr   = null,
+    serial     = null;
 
-var lenr = 8;
-var lenc = 13;
-if (numLed === -1) {
-    numLed = lenr * lenc;
-}
-var colorArr = new Array(lenr * lenc);
+function init() {
+    processCmdLineParams();
+    colorArr = new Array(numLed);
+    resetColors(colorArr, background);
 
-// zero padding a to b places
-function pad(a,b) {
-    return(1e15+a+"").slice(-b)
-}
-
-function fullColorSet(arr) {
-    var msg = '';
-    for (var i = 0; i < arr.length; i++) {
-        msg += pad(i, 3) + ':' + arr[i] + ',';
+    if (debug === false) {
+        openSerialConnection( getSerialPortDevice() );
     }
-    msg = msg.substring(0, msg.length-1);
-    return msg;
+    openSocketIOConnection();
 }
 
-/////////////////// Serial port with Arduino
+function processCmdLineParams() {
+    console.log("Starting socket.io server...");
+    var idx = process.argv.indexOf("--port");
+    if (idx >= 0 && idx < process.argv.length-1) {
+        port = parseInt( process.argv[idx + 1] );
+        console.log("Port " + port.toString());
+    }
+    var idx = process.argv.indexOf("--num-led");
+    if (idx >= 0 && idx < process.argv.length-1) {
+        numLed = parseInt( process.argv[idx + 1] );
+        console.log("Number of LEDs in chain: " + numLed.toString());
+    } else {
+        numLed = lenr * lenc;
+    }
+    var idx = process.argv.indexOf("--debug");
+    if (idx >= 0 && idx < process.argv.length) {
+        debug = true;
+        console.log("Debug mode");
+    }
+}
 
 function getSerialPortDevice() {
     var device = '/dev/ttyACM0';
@@ -62,96 +67,131 @@ function getSerialPortDevice() {
     return device;
 }
 
-// open serial connection
-var serial = new serialPort.SerialPort(getSerialPortDevice(), {
-  baudrate: 9600
-});
-
-serial.on('open', function(error) {
-    if (error) {
-        console.log('RPi: Port failed to open: '+error);
-    } else {
-        console.log('RPi: Serial port opened');
-    }
-
-    // react to arduino sensed events
-    serial.on('data', function(data) {
-        console.log( 'Arduino: ' + data.toString('ascii') );
+function openSerialConnection(device) {
+    serial = new serialPort.SerialPort(device, {
+      baudrate: 9600
     });
 
-    serial.on('close', function(data) {
-        console.log( 'RPi: Serial port closed: ' + JSON.stringify(data) );
-    });
+    serial.on('open', function(error) {
+        if (error) {
+            console.log('RPi: Port failed to open: '+error);
+        } else {
+            console.log('RPi: Serial port opened');
+        }
 
-    serial.on('error', function(error) {
-        console.log( 'RPi: Serial port error: ' + JSON.stringify(error) );
-    });
+        // react to arduino sensed events
+        serial.on('data', function(data) {
+            console.log( 'Arduino: ' + data.toString('ascii') );
+        });
 
-});
+        serial.on('close', function(data) {
+            console.log( 'RPi: Serial port closed: ' + JSON.stringify(data) );
+        });
+
+        serial.on('error', function(error) {
+            console.log( 'RPi: Serial port error: ' + JSON.stringify(error) );
+        });
+
+    });
+}
+
+// Communication API
+// 'initial_state', 'local_update', 'status_query' => 'remote_updates', 'remote_update'
+function openSocketIOConnection() {
+    io.on('connection', function(socket) {
+        console.log('connected');
+
+        // request for initial set of data
+        socket.on('initial_state', function(data) {
+            console.log('initial_state');
+            socket.emit('remote_updates', fullColorSet(colorArr));
+        });
+
+        // clear all of the leds
+        socket.on('clear_state', function(data) {
+            console.log('clear_state');
+            resetColors(colorArr, background);
+            io.emit('remote_updates', fullColorSet(colorArr));
+        });
+
+        // update the color of a particular led
+        socket.on('local_update', function(colormsg) {
+            console.log('local_update: ' + colormsg);
+            // save state
+            var ind = parseInt( colormsg.substring(0, 3) );
+            colorArr[ind] = colormsg.substring(4, colormsg.length);
+            // set color, forward to other clients
+            io.emit('remote_update', colormsg);
+            sendColor(msg);
+        });
+
+        // request for status of a particular led
+        socket.on('status_query', function(data) {
+            console.log('status_query: ' + data);
+            socket.emit('remote_update', getColor(data));
+        });
+
+        socket.on('disconnect', function(data) {
+            console.log("disconnected: " + JSON.stringify(data));
+        });
+        socket.on('error', function(data) {
+            console.log("errored: " + JSON.stringify(data));
+        });
+
+    });
+}
 
 // incoming = '001:#AA44FF'
 // outgoing = '^001:AA44FF\n'
-var sendColor = function( colormsg ) {
+// ignore message if out-of-bounds
+// don't send over serial if debug mode
+function sendColor(colormsg) {
+    if (debug === true) {
+        console.log(colormsg);
+    }
     var index = colormsg.substring(0, 3);
     var colorhex = colormsg.substring(5, colormsg.length);
-    colorArr[parseInt( index )] = colorhex;
 
-    serial.write('^' + index + ':' + colorhex + '\n');
-    serial.drain();
-    sleep.sleep(0.05)
+    if (parseInt(index) < numLed) {
+        colorArr[parseInt( index )] = colorhex;
+        if (debug === false) {
+            serial.write('^' + index + ':' + colorhex + '\n');
+            serial.drain();
+            sleep.sleep(0.05)
+        }
+    }
 }
 
-function resetColors(arr) {
+function getColor(ind) {
+    ind = parseInt(ind);
+    var msg = background;
+    if (ind < numLed) {
+        msg = colorArr[ind];
+    }
+    return ind.toString() + ':' + msg;
+}
+
+function resetColors(arr, backgroundColor) {
     for (var i = 0; i < arr.length; i++) {
-        arr[i] = '#22CCCC';
+        arr[i] = backgroundColor;
         sendColor(arr[i]);
     }
 }
 
-// set the initial state of the table
-resetColors(colorArr);
+// zero padding a to b places
+function pad(a,b) {
+    return(1e15+a+"").slice(-b)
+}
 
-//////////////////// Communication API
-// 'initial_state', 'local_update' => 'remote_updates', 'remote_update'
-
-io.on('connection', function(socket) {
-    console.log('connected');
-
-    // request for initial set of data
-    socket.on('initial_state', function(data) {
-        console.log('initial_state');
-
-        socket.emit('remote_updates', fullColorSet(colorArr));
-    });
-
-    socket.on('clear_state', function(data) {
-        console.log('clear_state');
-
-        resetColors(colorArr);
-        io.emit('remote_updates', fullColorSet(colorArr));
-    });
-
-    socket.on('local_update', function(colormsg) {
-        console.log('local_update: ' + colormsg);
-        // save state
-        var ind = parseInt( colormsg.substring(0, 3) );
-        colorArr[ind] = colormsg.substring(4, colormsg.length);
-        
-        // set color, forward to other clients
-        io.emit('remote_update', colormsg);
-        sendColor(msg);
-    });
+function fullColorSet(arr) {
+    var msg = '';
+    for (var i = 0; i < arr.length; i++) {
+        msg += pad(i, 3) + ':' + arr[i] + ',';
+    }
+    msg = msg.substring(0, msg.length-1);
+    return msg;
+}
 
 
-    socket.on('disconnect', function(data) {
-        console.log("disconnected: " + JSON.stringify(data));
-    });
-
-    socket.on('error', function(data) {
-        console.log("errored: " + JSON.stringify(data));
-    });
-
-});
-
-
+init();
 
--- static/table/table_client.js
+++ static/table/table_client.js
@@ -1,43 +1,37 @@
 (function(){
 
-    var _self = this;
-    var colorArr = [];
-    var table1 = null;
-    var clearbtn = null;
-    var lenr = 8;
-    var lenc = 13;
-    var pixelSize = 60;
-    var socketServer = window.location.protocol + '//raspled.com:8080';
-
-    // pad a with enough zeros to fill b digits
-    var pad = function(a,b) {
-        return (1e15+a+"").slice(-b)
-    }
-
-    if (!Date.now) {
-        Date.now = function() { return new Date().getTime(); };
-    }
+    var colorArr     = [],
+        table1       = null,
+        clearbtn     = null,
+        lenr         = 8,
+        lenc         = 13,
+        pixelSize    = 60,
+        socket       = null,
+        socketServer = window.location.protocol + '//rasp.com:8080';
     
     var init = function() {
+        colorArr = new Array(lenr * lenc);
+
+        // set debugging
+        //localStorage.debug='*';
+        localStorage.debug='';
+
+        createTable();
+        setTableDownListener();
+        openConnection();
+        if (socket != null) {
+            setClearBtn();
+            setTableUpListener();
+        }
+    }
+
+    var createTable = function() {
         // set the canvas
         table1 = new fabric.Canvas('table1', {
             selection: false,
             backgroundColor : '#222222'
         });
 
-        // set debugging
-        //localStorage.debug='*';
-        localStorage.debug='';
-
-        clearbtn = $('#clearbtn');
-
-        tableSetup();
-        connectionSetup();
-    }
-
-    var tableSetup = function() {
-        // set array of rects
-        colorArr = new Array(lenr * lenc);
         // add the tableIndex param to all fabric objects
         fabric.Object.prototype.tableIndex = -1;
         fabric.Object.prototype.selectedStart = -1;
@@ -69,55 +63,25 @@
                 table1.add(rect);
             }
         }
+        console.log('table created');
+    }
 
+    // sets mouse down listeners for color squares on the table
+    var setTableDownListener = function() {
         table1.on({
             'object:selected': function(e) {     // 'mouse:down'
                 if (e.target && e.target.get('tableIndex') !== -1) {
                     e.target.opacity = 0.5;
                     e.target.selectedStart = Date.now();
-
                     console.log('mouse down: ' + pad(e.target.get('tableIndex'), 3));
                     //table1.renderAll();
                 }
             }
         });
-
-        console.log('table created');
     }
 
-    // comm API
-    // 'initial_state', 'local_update' => 'remote_updates', 'remote_update'
-    var connectionSetup = function() {
-        // connect to the host server
-        var socket = io.connect(socketServer);
-
-        console.log('connection created');
-
-        socket.on('remote_update', function(colormsg) {
-            console.log('remote_update: ' + colormsg);
-
-            // update the state of the table based on remote change
-            var ind = parseInt( colormsg.substring(0, 3) );
-            var color = colormsg.substring(4, colormsg.length);
-            
-            colorArr[ind].set('fill', color);
-            colorArr[ind].set('opacity', 1);
-            table1.renderAll();            
-        });
-
-        socket.on('remote_updates', function(msg) {
-            console.log('remote_updates: ' + msg);
-
-            // set the initial state of the table
-            var initialColors = msg.split(',');
-            $.each(initialColors, function( index, elem ) {
-                var ind = parseInt( elem.substring(0, 3) );
-                var color = elem.substring(4, elem.length);
-                colorArr[ind].set('fill', color);
-            });
-            table1.renderAll();
-        });
-
+    // sets mouse up listeners for color squares on the table
+    var setTableUpListener = function() {
         // emit the state of the table based on local changes
         table1.on('mouse:up', function(e) {
             if (e.target && e.target.get('tableIndex') !== -1) {
@@ -125,7 +89,7 @@
 
                 var selectedTime = Date.now() - activeObj.selectedStart;
                 // use time to determine the color of the rect
-                console.log(JSON.stringify( selectedTime ));
+                console.log('msg sent: ' + JSON.stringify( selectedTime ));
                 activeObj.set('selectedStart', -1);
 
                 var objColor = '#FFAA33';
@@ -136,25 +100,64 @@
 
                 var msg = pad(activeObj.get('tableIndex'), 3) + ":" + objColor;
                 socket.emit('local_update', msg);
-                console.log('msg sent');
             }
         });
+    }
 
+    var setClearBtn = function() {
+        clearbtn = $('#clearbtn');
         clearbtn.click(function() {
-            socket.emit('clear_state', 'clearing colors...');
+            socket.emit('clear_state', 'clearing colors');
+        });
+    }
+
+    // comm API
+    // 'initial_state', 'local_update' => 'remote_updates', 'remote_update'
+    var openConnection = function() {
+        // connect to the host server
+        socket = io.connect(socketServer);
+
+        socket.on('remote_update', function(colormsg) {
+            console.log('remote_update: ' + colormsg);
+            // update the state of the table based on remote change
+            var ind = parseInt( colormsg.substring(0, 3) );
+            var color = colormsg.substring(4, colormsg.length);
+            colorArr[ind].set('fill', color);
+            colorArr[ind].set('opacity', 1);
+            table1.renderAll();            
+        });
+
+        socket.on('remote_updates', function(msg) {
+            console.log('remote_updates: ' + msg);
+            // set the initial state of the table
+            var initialColors = msg.split(',');
+            $.each(initialColors, function( index, elem ) {
+                var ind = parseInt( elem.substring(0, 3) );
+                var color = elem.substring(4, elem.length);
+                colorArr[ind].set('fill', color);
+            });
+            table1.renderAll();
         });
 
         socket.on('disconnect', function(msg) {
             console.log('disconnected: ' + JSON.stringify(msg));
         });
-
         socket.on('error', function(msg) {
             console.log('errored: ' + JSON.stringify(msg));
         });
 
         // get initial state of table
         socket.emit('initial_state', null);
+        console.log('connection created');
+    }
 
+    // pad a with enough zeros to fill b digits
+    var pad = function(a,b) {
+        return (1e15+a+"").slice(-b)
+    }
+
+    if (!Date.now) {
+        Date.now = function() { return new Date().getTime(); };
     }
 
     init();
